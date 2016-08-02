bb142859d9e6ce291f5f9e8f56e3b06351d2b72c
Anudle <AnuShrestha007@gmail.com>
2016-08-02 09:54:36 -0600

    updated picture

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
 $(document).ready(function() {
-var test1;
 
-  $('#teams').change(function(event) {
-                event.preventDefault();
+        $('#teams').change(function(event) {
+            event.preventDefault();
 
-                var selection = $('#teamsSelect').val();
-                console.log(selection)
+            var selection = $('#teamsSelect').val();
+            console.log(selection)
 
-                $.ajax({
-                    url: 'https://www.reddit.com/r/nfl/comments/'+ selection + '.json' ,
+            $.ajax({
+                    url: 'https://www.reddit.com/r/nfl/comments/' + selection + '.json',
 
                     error: function(err) {
                         console.error(err)
                     },
                     method: 'GET',
                     success: function(data) {
-                      var comments = data[1].data.children
+
+                            var comments = data[1].data.children
 
                             function insult() {
-
-
                                 var randomNum = Math.floor(Math.floor(Math.random() * 30))
                                 for (i = randomNum; i < comments.length; i++)
 
-                                    if (comments[i].data.score > 200) { //SCORE OF 200 UPVOTES
-                                        //console.log(i)
-                                        console.log(comments[i].data.body)
-                                        //console.log(comments[i].data.score);
+                                    if ((comments[i].data.score > 200)&&(comments[i].data.body.slice(0, 4) == "http")) { //SCORE
+                                    // console.log(i)
+                                     console.log(comments[i].data.body_html)
+                                    // console.log(comments[i].data.score);
+                                     console.log(comments[i].data.body.slice(0, 4))
 
-                                        var varPicTest = (comments[i].data.body.slice(0,4))
-                                        console.log(varPicTest)
+                                    $('#comment').html("<iframe src=" + comments[i].data.body + " height=600px width=600px>")
+                                    $('#author').html("said by " + comments[i].data.author + ", a " + comments[i].data.author_flair_text + " fan")
 
-                                        if (varPicTest == "http:" || "https"){
+                                    break
 
-                                        $('#pic').append("<img src= 'comments[i].data.body' >")
+                                } else if (comments[i].data.score > 200) {
+                                    console.log(comments[i].data.body)
+                                    $("#comment").html(comments[i].data.body)
+                                    $('#author').html("said by " + comments[i].data.author + ", a " + comments[i].data.author_flair_text + " fan")
 
-                                        }else{
+                                    break
 
-                                        $("#comment").html(comments[i].data.body)
-                                        $('#author').html("said by " + comments[i].data.author + ", a " + comments[i].data.author_flair_text + " fan")
-                                        $('#pic').append("<img src='comments[i].data.body")
-                                        break
-                                      }
-                                    }
+                                }
 
-                                    // if (comments[i].data.body == 'http:'){$('#pic').append("<img src='comments[i].data.body")
-                                    //  }
 
-                                  }
+                            }
 
 
 
@@ -59,5 +54,5 @@ var test1;
 
                         } //closes success function
                 }) // closes ajax function
-              })
+        })
     }) //closes ready function
